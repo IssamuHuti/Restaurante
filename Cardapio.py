97afa3cdb6db0d70f991897942857879e85d8631
IssamuHuti <157076012+IssamuHuti@users.noreply.github.com>
2024-08-11 10:05:20 -0300

    atualização 11/08 cardápio

--- Cardapio.py
+++ Cardapio.py
@@ -8,7 +8,9 @@ def ingrediente_usado():
     insira_ingrediente = input('Insira ingrediente para o prato: ')
     if insira_ingrediente.lower() == 'pare':
         return None, None
-    medida_ingrediente = input('Medida do ingrediente: ')
+    medida = input('Medida do ingrediente: ')
+    unidade = input('Unidade de medida: ')
+    medida_ingrediente = [medida, unidade]
     return insira_ingrediente, medida_ingrediente
 
 def obter_ingrediente():
@@ -26,22 +28,22 @@ while True:
 
     ingredientes = {}
 
-    ingrediente, medida = obter_ingrediente()
+    ingrediente, medidas = obter_ingrediente()
     if ingrediente is None:
         break
-    ingredientes.update({ingrediente: medida})
+    ingredientes.update({ingrediente: medidas})
     while True:
         limpar()
-        ingrediente, medida = obter_ingrediente()
+        ingrediente, medidas = obter_ingrediente()
         if ingrediente is None:
             break
-        ingredientes.update({ingrediente: medida})
+        ingredientes.update({ingrediente: medidas})
     
     pratos.append((novo_prato, ingredientes))
 
     print(f'Prato: {novo_prato}')
-    for ingrediente, medida in ingredientes.items():
-        print(f'{ingrediente}: {medida}')
+    for ingrediente, medidas in ingredientes.items():
+        print(f'{ingrediente}: {medidas[0]} {medidas[1]}')
     
     break
 
