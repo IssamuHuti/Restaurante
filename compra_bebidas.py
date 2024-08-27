61ab10317835f266f1248746b60177a9dd9680dc
IssamuHuti <157076012+IssamuHuti@users.noreply.github.com>
2024-08-26 23:19:03 -0300

    26/08 atualização compra bebida

--- compra_bebidas.py
+++ compra_bebidas.py
@@ -13,10 +13,14 @@ print('Data da compra: ', data_formatada)
 print('Informe os gastos com bebidas do dia')
 
 bebidas = {}
+qtd_bebidas = {}
 
 bebida = input('Produto: ')
 preco_bebida = input('R$ ')
 bebidas.update({bebida: float(preco_bebida)})
+qtd_bebida = input('Quantidade: ')
+preco_unitario_bebida = round(float(preco_bebida) / int(qtd_bebida), 2)
+qtd_bebidas.update({bebida: [qtd_bebida, preco_unitario_bebida]})
 while True:
     limpar()
     bebida = input('Produto: ')
@@ -24,20 +28,34 @@ while True:
         break
     preco_bebida = input('R$ ')
     bebidas.update({bebida: float(preco_bebida)})
+    qtd_bebida = input('Quantidade: ')
+    preco_unitario_bebida = round(float(preco_bebida) / int(qtd_bebida), 2)
+    qtd_bebidas.update({bebida: [qtd_bebida, preco_unitario_bebida]})
 
 total_gasto_bebidas = round(sum(bebidas.values()), 2)
 
 arquivo_bebidas = {'Data': data_formatada, 'Total de gasto': total_gasto_bebidas, 'Produtos': bebidas}
+estoque_bebidas = {'Data': data_formatada, 'Gasto por bebida': qtd_bebidas}
 
-pasta_salva = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'compra_bebidas')
+pasta_salva_gastos = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'compra_bebidas', 'gastos')
+pasta_salva_estoque = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'compra_bebidas', 'estoque')
 os.makedirs(os.path.dirname(os.path.abspath(__file__)), exist_ok=True)
 
-compra_bebida_dia = os.path.join(pasta_salva, 'compra_bebida_{}.json'.format(str(data_formatada)))
+compra_bebida_dia = os.path.join(pasta_salva_gastos, 'compra_bebida_{}.json'.format(str(data_formatada)))
+compra_bebida_estoque = os.path.join(pasta_salva_estoque, 'estoque_bebida_{}.json'.format(str(data_formatada)))
 
 limpar()
 with open(compra_bebida_dia, 'w', encoding='utf8') as arquivo:
     json.dump(
         arquivo_bebidas,
+        arquivo,
+        ensure_ascii=False,
+        indent=2,
+    )
+
+with open(compra_bebida_estoque, 'w', encoding='utf8') as arquivo:
+    json.dump(
+        estoque_bebidas,
         arquivo,
         ensure_ascii=False,
         indent=2,
