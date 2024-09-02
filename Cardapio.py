c65af7210780f2cba52c709d9de70b898d45931d
IssamuHuti <157076012+IssamuHuti@users.noreply.github.com>
2024-09-01 23:24:33 -0300

    atualização 01/09

--- Cardapio.py
+++ Cardapio.py
@@ -10,62 +10,83 @@ def ingrediente_usado():
         return None, None
     medida = input('Medida do ingrediente: ')
     unidade = input('Unidade de medida: ')
-    medida_ingrediente = [medida, unidade]
+    medida_ingrediente = {'Medida': medida, 'Unidade': unidade}
     return insira_ingrediente, medida_ingrediente
 
 def obter_ingrediente():
     ingrediente, medida = ingrediente_usado()
     return ingrediente, medida
 
+def composicao_prato():
+    print(f'Prato: {novo_prato}')
+    for ingrediente, medidas in ingredientes.items():   
+        print(f'{ingrediente}: {medidas}')
+
 limpar()
 
 print('Cardápio do restaurante')
-pratos = {}
-while True:
-    novo_prato = input('Informe o nome do novo prato (ou "pare" para finalizar): ')
-    if novo_prato.lower() == 'pare':
-        break
 
-    ingredientes = {}
-
-    ingrediente, medidas = obter_ingrediente()
-    if ingrediente is None:
-        break
-    ingredientes.update({ingrediente: medidas})
-    while True:
-        limpar()
-        ingrediente, medidas = obter_ingrediente()
-        if ingrediente is None:
-            break
-        ingredientes.update({ingrediente: medidas})
-    
-    pratos.update({novo_prato: ingredientes})
-
-    limpar()
-    mais_um_prato = input('Deseja cadastrar mais um prato? Sim (S) Não (N)')
-    while mais_um_prato.upper() != 'S' and mais_um_prato.upper() != 'N':
-        print('Informe Sim (S) ou Não (N)')
-        mais_um_prato = input('Deseja cadastrar mais um prato? Sim (S) Não (N): ')
-    if mais_um_prato.upper() == 'S':
+cadastrar_vizualizar_cardapio = input('Deseja cadastrar (C) novo prato ou visualizar (V) o cardápio? ')
+while cadastrar_vizualizar_cardapio.upper() != 'C' and cadastrar_vizualizar_cardapio.upper() != 'V':
+    print('Informe (C) ou (V)')
+    cadastrar_vizualizar_cardapio = input('Deseja cadastrar (C) novo prato ou visualizar (V) o cardápio? ')
+    if cadastrar_vizualizar_cardapio.upper() == 'C':
         limpar()
         continue
     else:
         break
 
-print(f'Prato: {novo_prato}')
-for ingrediente, medidas in ingredientes.items():
-        print(f'{ingrediente}: {medidas[0]} {medidas[1]}')
+caminho_cardapio_json = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'cardapio')
 
-pasta_cardapio = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'cardapio')
-os.makedirs(pasta_cardapio, exist_ok=True)
+if cadastrar_vizualizar_cardapio.upper() == 'C':
+    pratos = {}
+    while True:
+        novo_prato = input('Informe o nome do novo prato (ou "pare" para finalizar): ')
+        if novo_prato.lower() == 'pare':
+            break
 
-card = os.path.join(pasta_cardapio, 'cardapio.json')
+        ingredientes = {}
 
-with open(card, 'w', encoding='utf8') as arquivo:
-    json.dump(
-        pratos,
-        arquivo,
-        ensure_ascii=False,
-        indent=2,
-    )
+        ingrediente, medidas = obter_ingrediente()
+        if ingrediente is None:
+            break
+        ingredientes.update({ingrediente: medidas})
+        while True:
+            limpar()
+            ingrediente, medidas = obter_ingrediente()
+            if ingrediente is None:
+                break
+            ingredientes.update({ingrediente: medidas})
+        
+        pratos.update({novo_prato: ingredientes})
 
+        limpar()
+        mais_um_prato = input('Deseja cadastrar mais um prato? Sim (S) Não (N)')
+        while mais_um_prato.upper() != 'S' and mais_um_prato.upper() != 'N':
+            print('Informe Sim (S) ou Não (N)')
+            mais_um_prato = input('Deseja cadastrar mais um prato? Sim (S) Não (N): ')
+        if mais_um_prato.upper() == 'S':
+            limpar()
+            continue
+        else:
+            break
+
+    composicao_prato()
+
+    pasta_cardapio = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'cardapio')
+    os.makedirs(pasta_cardapio, exist_ok=True)
+
+    card = os.path.join(pasta_cardapio, 'cardapio.json')
+
+    with open(card, 'w', encoding='utf8') as arquivo:
+        json.dump(
+            pratos,
+            arquivo,
+            ensure_ascii=False,
+            indent=2,
+        )
+
+if cadastrar_vizualizar_cardapio.upper() == 'V':
+    with open(caminho_cardapio_json, 'r', encoding='utf8') as arquivo_cardapio:
+        dados_cardapio = json.load(arquivo_cardapio)
+        print(dados_cardapio)
